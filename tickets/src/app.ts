edef0c144a612313196e7c7fb59eacc9a588d71c
usama-pixel <usamaali052@gmail.com>
2024-08-09 17:20:29 +0500

    tickets service completed with tests

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -2,16 +2,25 @@ import express, { Request } from 'express'
 import 'express-async-errors'
 import { json } from 'body-parser'
 import cookieSession from 'cookie-session'
-import { errorHandler, NotFoundError } from '@uatickets/common'
+import { currentUser, errorHandler, NotFoundError } from '@uatickets/common'
+import { createTicketRouter } from './routes/new'
+import { showTicketRouter } from './routes/show'
+import { indexTicketRouter } from './routes'
+import { updateTicketRouter } from './routes/update'
 
 const app = express()
-app.set('trust proxy', true)
+app.set('trust proxy', true) // maybe important for letting frontend pod interact with this backend pod
 app.use(json())
 app.use(cookieSession({
     signed: false,
     secure: process.env.NODE_ENV !== 'test'
 }))
 
+app.use(currentUser)
+app.use(createTicketRouter)
+app.use(showTicketRouter)
+app.use(indexTicketRouter)
+app.use(updateTicketRouter)
 
 app.all('*', async () => { // app.all is a shortcut for app.get, app.post, app.put, app.delete
     // it will watch for any kind of method and route
--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -1,51 +1,52 @@
-import { MongoMemoryServer } from 'mongodb-memory-server';
-import mongoose from 'mongoose';
-import request from 'supertest';
-import { app } from '../app';
+import mongoose from "mongoose";
+import { MongoMemoryServer } from 'mongodb-memory-server'
+import request from 'supertest'
+import jwt from 'jsonwebtoken'
 
 declare global {
-  var signin: () => Promise<string[]>;
+    var signin: () => string[];
 }
 
-let mongo: any;
+// declare global {
+//     namespace NodeJS {
+//         interface Global {
+//             signin(): Promise<string[]>
+//         }
+//     }
+// }
+
+let mongo: any
 beforeAll(async () => {
-  process.env.JWT_KEY = 'asdfasdf';
-  process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
-
-  const mongo = await MongoMemoryServer.create();
-  const mongoUri = mongo.getUri();
-
-  await mongoose.connect(mongoUri, {});
-});
+    process.env.JWT_KEY = 'abc'
+    // process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+    mongo = await MongoMemoryServer.create()
+    const mongoUri = mongo.getUri()
+    await mongoose.connect(mongoUri, {})
+})
 
 beforeEach(async () => {
-  const collections = await mongoose.connection.db.collections();
-
-  for (let collection of collections) {
-    await collection.deleteMany({});
-  }
-});
+    const collections = await mongoose.connection.db.collections()
+    for (let collection of collections) {
+        await collection.deleteMany({})
+    }
+})
 
 afterAll(async () => {
-  if (mongo) {
-    await mongo.stop();
-  }
-  await mongoose.connection.close();
-});
+    if(mongo) {
+        await mongo.stop()
+    }
+    await mongoose.connection.close()
+})
 
-global.signin = async () => {
-  const email = 'test@test.com';
-  const password = 'password';
-
-  const response = await request(app)
-    .post('/api/users/signup')
-    .send({
-      email,
-      password,
-    })
-    .expect(201);
-
-  const cookie = response.get('Set-Cookie');
-
-  return cookie;
-};
+global.signin = () => {
+    const id = new mongoose.Types.ObjectId().toHexString()
+    const payload = {
+        id: id,
+        email: 'test@test.com'
+    }
+    const token = jwt.sign(payload, process.env.JWT_KEY!)
+    const session = { jwt: token }
+    const sessionJSON = JSON.stringify(session)
+    const base64 = Buffer.from(sessionJSON).toString('base64')
+    return [`session=${base64}`];
+}
